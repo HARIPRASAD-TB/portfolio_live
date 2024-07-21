502c8d055f2ad4b08d6ba7d873469b982e03ed8d
hariprasad <harih79562@gmail.com>
2024-07-21 20:21:46 +0530

    updating cv

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -21,7 +21,7 @@ function Projects() {
             <ProjectCard
               imgPath={moonstride}
               isBlog={false}
-              title="Tourengine"
+              title="Tourengine with Viator and 1Way2Italy"
               description="I developed and maintained an advanced tour booking website using React and Next.js, with a strong emphasis on enhancing the user interface and overall user experience. This project also involved implementing robust server-side logic and backend functionality using Node.js, ensuring seamless integration with the frontend, particularly through integration with Moonstride."
               demoLink="https://www.moonstride.com/"
             />
@@ -40,7 +40,7 @@ function Projects() {
             <ProjectCard
               imgPath={moonstride}
               isBlog={false}
-              title="Zoho"
+              title="Moonstride integration with Zoho books"
               description="As part of enhancing the accounting capabilities within Moonstride, I implemented an integration with Zoho Accounting Software. This integration streamlines the management of user accounts by synchronizing invoice and payment details between Moonstride and Zoho, ensuring accurate and efficient financial tracking."
               demoLink="https://www.moonstride.com/"
             />
@@ -50,7 +50,7 @@ function Projects() {
             <ProjectCard
               imgPath={moonstride}
               isBlog={false}
-              title="Xero"
+              title="Moonstride integration with Xero"
               description="I developed a seamless integration between Moonstride and Xero Accounting Software to enhance financial management for Moonstride users. This integration synchronizes invoice and payment details between the two platforms, ensuring accurate and efficient account management."
               demoLink="https://www.moonstride.com/"
             />
@@ -60,7 +60,7 @@ function Projects() {
             <ProjectCard
               imgPath={moonstride}
               isBlog={false}
-              title="Quick books"
+              title="Moonstride integration with Quick books"
               description="To streamline financial management for Moonstride users, I developed an integration with QuickBooks Accounting Software. This integration facilitates the synchronization of invoice and payment details between Moonstride and QuickBooks, ensuring efficient and accurate account management."
               demoLink="https://www.moonstride.com/"
             />
@@ -70,7 +70,7 @@ function Projects() {
             <ProjectCard
               imgPath={integral}
               isBlog={false}
-              title="Integral"
+              title="Web scraping with Integral"
               description="I developed a web scraping solution using Selenium in Python to collect and analyze data from Integral. This project involved automating the data extraction process and performing detailed analysis on the collected data to derive valuable insights."
               demoLink="https://www.integralmemory.com/"
             />
